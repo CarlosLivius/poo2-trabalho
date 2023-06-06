50a27ce9f6f35ecd33a93fca4739c80548da141c
bernardohmp <102256921+bernardohmp@users.noreply.github.com>
2023-06-06 15:37:17 -0300

    Update Comida.java

--- poo2-trabalho/Restaurante/src/Comida.java
+++ poo2-trabalho/Restaurante/src/Comida.java
@@ -2,4 +2,5 @@ public interface Comida {
     void preparar();
     void adicionarObserver(PedidoObserver observer);
     void removerObserver(PedidoObserver observer);
+    void MostrarAdicionais();
 }
